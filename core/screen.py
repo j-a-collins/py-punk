747fdf385eb6a8b3dbc729398fef15d1fb349881
J A Collins <45298131+j-a-collins@users.noreply.github.com>
2023-01-08 20:14:00 +0000

    Add files via upload

---
+++ core/screen.py
@@ -0,0 +1,26 @@
+'''
+doc string
+'''
+
+# Imports
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import time
+
+
+def screen_record():
+    last_time = time.time()
+    while(True):
+        printscreen = np.array(ImageGrab.grab(bbox=(0, 40, 1200, 600)))
+        print(f"Loop time={time.time()-last_time}")
+        last_time = time.time()
+        cv2.imshow('window', cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+if __name__ == "__main__":
+    screen_record()
+
+
